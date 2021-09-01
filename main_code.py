2a3035bd7be50c3d5c0041e9d01164bb3d7443c3
YuliyaGurieva <delo_pochta@mail.ru>
2021-09-01 11:12:07 +0300

    The window is created

--- main_code.py
+++ main_code.py
@@ -1,1 +1,12 @@
 import pygame
+
+size = (400, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Snake Python')
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
