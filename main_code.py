838dddcb5a83f8214fe58a5cfe8ad8c29446ff37
YuliyaGurieva <delo_pochta@mail.ru>
2021-09-01 20:46:15 +0300

    Before replacing mainlopp by another function

--- main_code.py
+++ main_code.py
@@ -1,8 +1,9 @@
 import pygame
 import sys
 import random
+import pygame_menu
 pygame.init()
-
+bg_image = pygame.image.load('snake.jpg')
 space_color = (0, 50, 0)
 header_color = (0, 100, 50)
 snake_color = (0, 100, 0)
@@ -28,83 +29,93 @@ class SnakeWay:
         return 0 <= self.x < count_blocks and 0 <= self.y < count_blocks
     def __eq__(self, other):
         return isinstance(other, SnakeWay) and self.x == other.x and self.y == other.y
-def new_food_coords():
-    x = random.randint(0, count_blocks - 1)
-    y = random.randint(0, count_blocks - 1)
-    new_food = SnakeWay(x, y)
-    while new_food in snake_ways:
-        new_food.x = random.randint(0, count_blocks - 1)
-        new_food.y = random.randint(0, count_blocks - 1)
-    return new_food
+
 def draw_block(color, row, column):
     pygame.draw.rect(screen, color, [size_block + column * size_block,
                                      header_field + size_block + row * size_block, size_block, size_block])
-snake_ways = [SnakeWay(9, 8), SnakeWay(9, 9), SnakeWay(9, 10)]
-food = new_food_coords()
-dif_row = key_row = 0
-dif_col = key_col = 1
-total = 0
-speed = 2
+def start_the_game():
+    def new_food_coords():
+        x = random.randint(0, count_blocks - 1)
+        y = random.randint(0, count_blocks - 1)
+        new_food = SnakeWay(x, y)
+        while new_food in snake_ways:
+            new_food.x = random.randint(0, count_blocks - 1)
+            new_food.y = random.randint(0, count_blocks - 1)
+        return new_food
+    snake_ways = [SnakeWay(9, 8), SnakeWay(9, 9), SnakeWay(9, 10)]
+    food = new_food_coords()
+    dif_row = key_row = 0
+    dif_col = key_col = 1
+    total = 0
+    speed = 2
 
-while True:
+    while True:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print('exit')
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and dif_col != 0:
-                key_row = -1
-                key_col = 0
-            elif event.key == pygame.K_DOWN and dif_col != 0:
-                key_row = 1
-                key_col = 0
-            elif event.key == pygame.K_LEFT and dif_row != 0:
-                key_row = 0
-                key_col = -1
-            if event.key == pygame.K_RIGHT and dif_row != 0:
-                key_row = 0
-                key_col = 1
-    screen.fill(space_color)
-    pygame.draw.rect(screen, header_color, (0, 0, size[0], header_field))
-    text_total = font_total.render(f'Your progress: {total}', False, green)
-    text_speed = font_total.render(f'Speed: {speed}', False, green)
-    screen.blit(text_total, (size_block, size_block))
-    screen.blit(text_speed, (size_block + 250, size_block))
-    for row in range(count_blocks):
-        for column in range(count_blocks):
-            if (row + column) % 2 == 0:
-                color = wheat
-            else:
-                color = black
-            draw_block(color, row, column)
-    head = snake_ways[-1]
-    if not head.is_inside():
-        print('GAME OVER!', 'You are failed!', sep='\n')
-        pygame.quit()
-        sys.exit()
-    draw_block(red, food.x, food.y)
-    for block in snake_ways:
-        draw_block(snake_color, block.x, block.y)
-    pygame.display.flip()
-    if food == head:
-        total += 1
-        if total % 5 == 0:
-            speed += 1
-        snake_ways.append(food)
-        food = new_food_coords()
-    dif_row = key_row
-    dif_col = key_col
-    new_head = SnakeWay(head.x + dif_row, head.y + dif_col)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print('exit')
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and dif_col != 0:
+                    key_row = -1
+                    key_col = 0
+                elif event.key == pygame.K_DOWN and dif_col != 0:
+                    key_row = 1
+                    key_col = 0
+                elif event.key == pygame.K_LEFT and dif_row != 0:
+                    key_row = 0
+                    key_col = -1
+                if event.key == pygame.K_RIGHT and dif_row != 0:
+                    key_row = 0
+                    key_col = 1
+        screen.fill(space_color)
+        pygame.draw.rect(screen, header_color, (0, 0, size[0], header_field))
+        text_total = font_total.render(f'Your progress: {total}', False, green)
+        text_speed = font_total.render(f'Speed: {speed}', False, green)
+        screen.blit(text_total, (size_block, size_block))
+        screen.blit(text_speed, (size_block + 250, size_block))
+        for row in range(count_blocks):
+            for column in range(count_blocks):
+                if (row + column) % 2 == 0:
+                    color = wheat
+                else:
+                    color = black
+                draw_block(color, row, column)
+        head = snake_ways[-1]
+        if not head.is_inside():
+            print('GAME OVER!', 'You are failed!', sep='\n')
+            break
+        draw_block(red, food.x, food.y)
+        for block in snake_ways:
+            draw_block(snake_color, block.x, block.y)
+        pygame.display.flip()
+        if food == head:
+            total += 1
+            if total % 5 == 0:
+                speed += 1
+            snake_ways.append(food)
+            food = new_food_coords()
+        dif_row = key_row
+        dif_col = key_col
+        new_head = SnakeWay(head.x + dif_row, head.y + dif_col)
 
-    if new_head in snake_ways:
-        print('GAME OVER!', 'You crashed yourself!', sep='\n')
-        pygame.quit()
-        sys.exit()
-    snake_ways.append(new_head)
-    snake_ways.pop(0)
+        if new_head in snake_ways:
+            print('GAME OVER!', 'You crashed yourself!', sep='\n')
+            break
+        snake_ways.append(new_head)
+        snake_ways.pop(0)
 
 
-    timer.tick(speed)
+        timer.tick(speed)
+
+
+
+menu = pygame_menu.Menu('Welcome', 400, 300,
+                       theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add.text_input('Name: ', default='Guest')
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+menu.mainloop(screen)
 
